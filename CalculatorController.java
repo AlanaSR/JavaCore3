18ae7a0e72e47df4a045f99904c42ec6395a87ae
AlanaSR <svetlanka.shkola@gmail.com>
2024-09-06 23:57:11 +0500

    hw

--- CalculatorController.java
+++ CalculatorController.java
@@ -1,20 +1,41 @@
 package com.alanasr.alanasrspringdemo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/calculator")
 @RestController
 public class CalculatorController {
-    @GetMapping(path = "/calculator")
+    private final CalculatorServiseInterface calculatorService;
+    public CalculatorController (CalculatorServiseInterface calculatorService){
+        this.calculatorService=calculatorService;
+    }
+
+    @GetMapping()
     public String helloCalculator() {
-        return "Добро пожаловать в калькулятор";
+        return calculatorService.helloCalculator();
     }
 
-    @GetMapping(path = "/calculator")
-    public String sumNum(@RequestParam("plus") int num1, int num2) {
-        int sum = num1 + num2;
-        return num1 + " + " + num2 + " = " + sum;
+    @GetMapping(path = "/plus")
+    public String addition(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.addition(num1, num2);
     }
 
+    @GetMapping(path = "/minus")
+    public String subtraction(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.subtraction(num1, num2);
+    }
+
+    @GetMapping(path = "/multiply")
+    public String multiplication(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.multiplication(num1, num2);
+    }
+
+    @GetMapping(path = "/divide")
+    public String division(@RequestParam(value = "num1", required = false) Integer num1, @RequestParam(value = "num2", required = false) Integer num2) {
+        return calculatorService.division(num1, num2);
+    }
 }
+
